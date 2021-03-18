61200a0880931760a85c351fdbea806de31cb9ce
joaoofreitas <joaoofreitas00@gmail.com>
2021-03-18 01:18:19 +0000

    Parsing added. Need optimization and regex hotfixes ☕.

--- main.go
+++ main.go
@@ -5,33 +5,80 @@ import (
 	"io/ioutil"
 	"net/http"
 	"regexp"
+	"strings"
 )
 
+type Exercise struct {
+	name        string
+	description string
+}
+
+type Subject struct {
+	name      string
+	exercises []Exercise
+}
+
+func parseMD(text string) []string { // Can be optimized in terms of not even store the first introductory part of the README
+	var sections []string
+	var tmp string
+
+	re := regexp.MustCompile(`(?m)^-{3,}`)
+	textToArray := strings.Split(strings.Replace(text, "\r\n", "\n", -1), "\n")
+	textToArray = append(textToArray, "\n----")
+
+	for _, line := range textToArray {
+		if !re.MatchString(line) {
+			tmp += line + "\n"
+		} else {
+			sections = append(sections, tmp)
+			tmp = ""
+		}
+	}
+	return sections
+}
+
 func main() {
+	var listOfSubjects []Subject
+
 	res, err := http.Get("https://raw.githubusercontent.com/karan/Projects/master/README.md")
 	if err != nil {
 		print(err)
 	}
 
 	defer res.Body.Close()
+
 	body, err := ioutil.ReadAll(res.Body)
 	if err != nil {
 		print(err)
 	}
+
 	rawMD := string(body)
+	rawMDList := parseMD(rawMD)
 
 	subjectRegex := regexp.MustCompile(`([\r\n].*?)(:=?\r|\n)(..?(?:------).*)`)
-	for i, match := range subjectRegex.FindAllString(rawMD, -1) {
-		fmt.Println(match, "found at index", i)
-	}
 
-	var nameRegex = regexp.MustCompile(`(?m)^\*\*.*\*\*`)
-	for i, match := range nameRegex.FindAllString(rawMD, -1) {
-		fmt.Println(match, "found at index", i)
-	}
+	for i, subjectMatches := range subjectRegex.FindAllString(rawMD, -1) {
+		var listOfExercises []Exercise
+		var nameMatches, descriptionMatches []string
 
-	var descriptionRegex = regexp.MustCompile(`(?m)^\*\*.*\*`)
-	for i, match := range descriptionRegex.FindAllString(rawMD, -1) {
-		fmt.Println(match, " match found at index", i)
+		nameRegex := regexp.MustCompile(`(?m)^\*\*.*\*\*`)
+		descriptionRegex := regexp.MustCompile(`(?m)^\*\*.*\*`) //Fix this regex
+
+		nameMatches = nameRegex.FindAllString(rawMDList[i+1], -1)
+		descriptionMatches = descriptionRegex.FindAllString(rawMDList[i+1], -1)
+
+		if len(nameMatches) != len(descriptionMatches) {
+			panic("Parsing Error...")
+		}
+
+		for j := 0; j < len(nameMatches); j++ {
+			var exercise Exercise
+			exercise.name = nameMatches[j]
+			exercise.description = descriptionMatches[j]
+			listOfExercises = append(listOfExercises, exercise)
+		}
+		subject := Subject{name: subjectMatches, exercises: listOfExercises}
+		listOfSubjects = append(listOfSubjects, subject)
 	}
+	fmt.Printf("%+v\n", listOfSubjects)
 }
